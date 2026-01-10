ee603567b321990809ccc39cfda49a1e0e0296bf
tateakasledgehammer <tatewilliams29@gmail.com>
2026-01-10 12:46:24 +1030

    update to align with netlify

--- functions/subscribe.js
+++ functions/subscribe.js
@@ -1,5 +1,10 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("./firebase-service-account.json");
+
+// const serviceAccount = require("./firebase-service-account.json");"
+
+const serviceAccount = JSON.parse(
+    process.env.FIREBASE_SERVICE_ACCOUNT
+);
 
 if (!admin.apps.length) {
     admin.initializeApp({
